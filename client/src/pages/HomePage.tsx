ebefecd40b42af213e289c84518afbf44cf398e8
afeDayo <afedayo@gmail.com>
2026-05-19 13:41:24 +0100

    cart

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Man from "../components/Man";
+import Cart from "../components/Cart";
 
 const HomePage: React.FC = () => {
   return (
     <div>
+      <Cart />
       <Man />
     </div>
   );
