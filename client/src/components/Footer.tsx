643d6021780634d7187fee0d214395e5814654de
Tochi-Learner <tochitoks2010@gmail.com>
2026-05-25 07:29:07 -0700

    feat:created footer component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { IoLogoFacebook } from "react-icons/io";
+import { ImTwitter } from "react-icons/im";
+import { IoLogoInstagram } from "react-icons/io5";
 
 const Footer: React.FC = () => {
-  return <div></div>;
+  return;
+  <footer className=" bg-[#101010] py-12 px-6">
+    <div>
+      <h2>audiophile</h2>
+      HOME HEADPHONES SPEAKERS EARPHONES
+      <p></p>
+      <p></p>
+      <div className="text-[#FFFFFF] flex flex-row gap-6">
+        <IoLogoFacebook />
+        <ImTwitter />
+        <IoLogoInstagram />
+      </div>
+    </div>
+  </footer>;
 };
 
 export default Footer;
